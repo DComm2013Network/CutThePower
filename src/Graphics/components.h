c2322e683dd7c7982a105affc81a0027f45b6548
msiwoski <siwoski.mateusz@gmail.com>
2014-02-27 16:39:27 -0800

    fixed integration of components.h and deleted graphicscomponents.h (all integration tests)

--- src/Graphics/components.h
+++ src/Graphics/components.h
@@ -1,12 +1,15 @@
-#ifndef COMPONENT_H
-#define COMPONENT_H
-#include "graphicscomponents.h"
+#ifndef GRAPHIC_COMPONENT_H
+#define GRAPHIC_COMPONENT_H
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+/*
 typedef enum{
 	COMPONENT_NONE = 0,
 	COMPONENT_RENDER_PLAYER = 1 << 0,
 } Components;
-
+*/
 
 typedef struct{
 	SDL_Texture *playerTexture;
--- src/Graphics/graphicscomponents.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPHICSCOMPONENTS_H
-#define GRAPHICSCOMPONENTS_H
-
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-
-#endif
