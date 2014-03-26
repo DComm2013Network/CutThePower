24ab1a1a4d1558dc71d14f3d79b4dbcc8532a74b
Shane-S <shane.d.spoor@gmail.com>
2014-03-26 14:44:27 -0700

    Maybe fixed

--- src/Network/ClientUpdateSystem.cpp
+++ src/Network/ClientUpdateSystem.cpp
@@ -76,7 +76,6 @@ int client_update_system(World *world, int net_pipe) {
 	{
 
 		packet = read_data(net_pipe, &type);
-
 		if(floor_change_flag == 1)
 		{
 			switch(type)
@@ -186,11 +185,12 @@ void client_update_pos(World *world, void *packet)
 		
 		if(player_table[i] != UNASSIGNED)
 		{
-			if(world->position[player_table[i]].level != pos_update->floor)
+			if(!pos_update->players_on_floor[i])
 			{
 				world->mask[player_table[i]] &= ~(COMPONENT_RENDER_PLAYER | COMPONENT_COLLISION); // If the player is no longer on the floor, turn off render and collision
 				continue;
 			}
+			world->mask[player_table[i]] &= COMPONENT_RENDER_PLAYER | COMPONENT_COLLISION;
 			world->movement[player_table[i]].movX	= pos_update->xVel[i];
 			world->movement[player_table[i]].movY 	= pos_update->yVel[i];
 			world->position[player_table[i]].x		= pos_update->xPos[i];
