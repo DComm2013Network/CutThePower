5f963cff389010cb79a8a8b79c70151caf96107e
wrath-of-kant <keith.tang.13@hotmail.com>
2014-03-29 18:40:36 -0700

    STUFF

--- src/Graphics/fow.h
+++ src/Graphics/fow.h
@@ -4,31 +4,6 @@
 #include <SDL2/SDL.h>
 #include "../world.h"
 
-#define NUMLEVELS 8
 
-typedef struct FowTile
-{
-	SDL_Rect rect;
-	int visible[NUMLEVELS];
-} FowTile;
-
-
-typedef struct FowComponent
-{
-	struct fowtile **tiles;
-	int xOffset;
-	int yOffset;
-	SDL_Surface **fogOfWar;
-	SDL_Surface **alphaFog;
-	SDL_Surface  *corners[12];
-} FowComponent;
-
-
-typedef struct FowPlayerPosition
-{
-	World *world;
-	FowComponent *fow;
-	PositionComponent *pos;
-} FowPlayerPosition;
 
 #endif
