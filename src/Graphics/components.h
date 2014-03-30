644fea94651c024d9e4a29bbcc837caa36bb8803
wrath-of-kant <keith.tang.13@hotmail.com>
2014-03-29 18:32:20 -0700

    new file added

--- src/Graphics/components.h
+++ src/Graphics/components.h
@@ -39,32 +39,7 @@ typedef struct {
 	
 } Animation;
 
-#define NUMLEVELS 8
 
-typedef struct FowTile
-{
-	SDL_Rect rect;
-	int visible[NUMLEVELS];
-} FowTile;
-
-
-typedef struct FowComponent
-{
-	struct fowtile **tiles;
-	int xOffset;
-	int yOffset;
-	SDL_Surface **fogOfWar;
-	SDL_Surface **alphaFog;
-	SDL_Surface  *corners[12];
-} FowComponent;
-
-
-typedef struct FowPlayerPosition
-{
-	World *world;
-	FowComponent *fow;
-	PositionComponent *pos;
-} FowPlayerPosition;
 
 /**
  * Contains the components related to animation.
