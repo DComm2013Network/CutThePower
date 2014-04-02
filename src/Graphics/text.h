4e3826b47d39405d5c5dee5af04fbb854a8b6aab
jmarling <jmarling@hotmail.com>
2014-04-02 10:37:50 -0700

    Chat colours/sizes updated

--- src/Graphics/text.h
+++ src/Graphics/text.h
@@ -4,12 +4,18 @@
 #include <SDL2/SDL.h>
 #include "../world.h"
 
+#define	TITLE_FONT			0
+#define MENU_FONT			1
+#define CHAT_FONT			2
+#define PLAYER_FONT			3
+#define OTHER_TEAM_FONT		4
+#define SERVER_FONT			5
+
 void init_fonts();
 void cleanup_fonts();
-SDL_Surface *draw_big_text(const char *text);
-SDL_Surface *draw_small_text(const char *text);
-void render_big_text(World *world, unsigned int entity, const char *text);
-void render_small_text(World *world, unsigned int entity, const char *text);
-int get_small_text_width(const char *text);
+void render_text(World *world, unsigned int entity, const char *text, int font_type);
+SDL_Surface *draw_text(const char *text, int font_type);
+int get_text_width(const char *text, int font_type);
+int get_text_height(const char *text, int font_type);
 
 #endif
